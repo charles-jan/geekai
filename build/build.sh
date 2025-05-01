edba68cc618494bc2ced2cbdd2c01a1259383dda
xiazijian <527091041@qq.com>
2025-05-01 21:29:59 +0800

    infrastructure(personal): update Docker registry and configure new domain
    - Update Docker registry to personal CR Aliyuncs
    - Change container names and images in docker-compose.yaml
    - Update Dockerfiles for API and web components
    - Configure new domain in Nginx server block

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,7 @@
 
 version=$1
 arch=${2:-amd64}
+registry="crpi-1rhmnyrson8h7iaj.ap-southeast-1.personal.cr.aliyuncs.com/charles_jan"
 
 # build go api program
 cd ../api
@@ -14,15 +15,16 @@ npm run build
 cd ../build
 
 # remove docker image if exists
-docker rmi -f registry.cn-shenzhen.aliyuncs.com/geekmaster/geekai-api:$version-$arch
+docker rmi -f $registry/geekai-api:$version-$arch
+
 # build docker image for geekai-go
-docker build -t registry.cn-shenzhen.aliyuncs.com/geekmaster/geekai-api:$version-$arch -f dockerfile-api-go ../
+docker build -t $registry/geekai-api:$version-$arch -f dockerfile-api-go ../
 
 # build docker image for geekai-web
-docker rmi -f registry.cn-shenzhen.aliyuncs.com/geekmaster/geekai-web:$version-$arch
-docker build -t registry.cn-shenzhen.aliyuncs.com/geekmaster/geekai-web:$version-$arch -f dockerfile-web-$arch ../
+docker rmi -f $registry/geekai-web:$version-$arch
+docker build -t $registry/geekai-web:$version-$arch -f dockerfile-web-$arch ../
 
-if [ "$3" = "push" ];then
-  docker push registry.cn-shenzhen.aliyuncs.com/geekmaster/geekai-api:$version-$arch
-  docker push registry.cn-shenzhen.aliyuncs.com/geekmaster/geekai-web:$version-$arch
+if [ "$3" = "push" ]; then
+  docker push $registry/geekai-api:$version-$arch
+  docker push $registry/geekai-web:$version-$arch
 fi
